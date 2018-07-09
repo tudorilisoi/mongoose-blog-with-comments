a4dd440db4ae1a7987ba557f270ec20de3afdb92
Tudor Ilisoi <tudorilisoi@yahoo.com>
2018-07-09 08:53:05 +0300

    passing CR from blog post CRUd; stable; incomplete

--- server/api/blog/blog.test.js
+++ server/api/blog/blog.test.js
@@ -1,35 +1,103 @@
-const chai = require('chai');
-const chaiHttp = require('chai-http');
+const chai = require('chai')
+const chaiHttp = require('chai-http')
 
-const mongoose = require('mongoose');
-//const faker = require('faker');
-const { closeServer, runServer, app } = require('../../server');
-const { TEST_DATABASE_URL } = require('../../../config');
+const mongoose = require('mongoose')
+const { closeServer, runServer, app } = require('../../server')
+const { TEST_DATABASE_URL, PORT } = require('../../../config')
+const { getConfig } = require('../../api/api')
+const PostModel = getConfig('blog').models.post
 
-const expect = chai.expect;
-const should = chai.should();
-chai.use(chaiHttp);
+const expect = chai.expect; //using the chai assertion library
+const should = chai.should()
+chai.use(chaiHttp)
 
-describe('blog API', function () {
+const mockData = [
+    { title: 'Post 1' },
+    { title: 'Post 2' },
+    { title: 'Post 3' },
+]
 
-    before(function () {
-        return runServer(TEST_DATABASE_URL);
-    });
 
-    after(function () {
-        return closeServer();
-    });
+async function deleteCollections(namesArr) {
+    // get existing collections
+    const collections = await mongoose.connection.db.collections()
 
-    describe('Root URL', function () {
+    // filter existing collections by name  
+    const filteredCollections = collections.filter(item => namesArr.includes(item.collectionName))
 
-        it('should respond with a status of 200 and HTML', function () {
-            return chai.request(app)
-                .get('/blog/posts')
-                .then(function (res) {
-                    expect(res).to.have.status(200);
-                    expect(res).to.be.html;
-                });
-        });
-    });
+    //drop the collections so we can start fresh every time this test runs
+    return await Promise.all(filteredCollections.map(c => c.remove()))
+}
 
-});
+describe('blog API routes', function () {
+
+    before(async () => {
+        await runServer(TEST_DATABASE_URL, PORT)
+        // await deleteCollections(['blogpostmodels'])
+    })
+
+    after(async () => {
+        await closeServer()
+    })
+
+
+    describe('POST /blog/post/create', () => {
+        it('should create a post', async () => {
+            const title = 'A post with an akward title: ' + Math.random()
+            const res = await chai
+                .request(app)
+                .post('/blog/post/create')
+                .send({ title })
+            expect(res).to.have.status(200)
+            expect(res).to.be.json;
+            expect(res.body.post).to.be.an('object');
+            expect(res.body.post.title).to.equal(title)
+            expect(res.body.post.id).to.not.be.undefined
+        })
+    })
+
+    describe('GET /blog/posts (no records)', () => {
+        it('should respond with JSON', async () => {
+            await deleteCollections(['blogpostmodels'])
+            const res = await chai.request(app).get('/blog/posts')
+            expect(res).to.have.status(200)
+            expect(res).to.be.json;
+
+            //perform a deep comparison 'deep.equal' because arrays are not equal by reference
+            expect(res.body.posts).to.deep.equal([])
+        })
+
+        it('should fail when the offset param is out of bounds', async () => {
+            const res = await chai.request(app).get('/blog/posts/10')
+            expect(res).to.be.json;
+            expect(res).to.have.status(500)
+        })
+
+
+    })
+
+    describe('GET /blog/posts (some records)', () => {
+        it('should respond with JSON', async () => {
+            await deleteCollections(['blogpostmodels'])
+            await Promise.all(mockData.map(item => PostModel.create(item)))
+            const res = await chai.request(app).get('/blog/posts')
+            expect(res).to.have.status(200)
+            expect(res).to.be.json;
+            expect(res.body.posts).to.be.an('array');
+            expect(res.body.posts).to.have.lengthOf(3)
+        })
+
+        it('should account for the offset param', async () => {
+            const res = await chai.request(app).get('/blog/posts/2')
+            expect(res).to.be.json;
+            expect(res).to.have.status(200)
+            expect(res.body.posts).to.be.an('array');
+            expect(res.body.posts).to.have.lengthOf(1)
+            expect(res.body.total).to.equal(3)
+        })
+
+
+    })
+
+
+})
--- server/api/blog/blogPostModel.js
+++ server/api/blog/blogPostModel.js
@@ -4,20 +4,25 @@ const mongoose = require('mongoose');
 // mongoose.Promise = global.Promise;
 
 const BlogPostSchema = new mongoose.Schema({
-     title: { type: String, required: true}, 
-     date: { type: Date, required: true},
-    
-});
+  title: { type: String, required: true },
+  // date: { type: Date, required: true },
+
+},
+  { timestamps: { createdAt: 'createdAt' } }
+);
 
 
-BlogPostSchema.methods.serialize = function() {
+BlogPostSchema.methods.serialize = function () {
   return {
     id: this._id,
     title: this.title,
-    date: this.date,   
+    createdAt: this.createdAt,
   };
 };
 
+// NOTE: mongoose will normalize the collection name: 
+// lowercase(pluralize('BlogPostModel')) = 'blogpostmodels'
+// when doing mongoimport use --collection 'blogpostmodels'
 const BlogPostModel = mongoose.model('BlogPostModel', BlogPostSchema);
 
 module.exports = BlogPostModel
