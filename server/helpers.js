3762a8981dd3e4ba2bc2d3eac42850b9ca0e34b6
Tudor Ilisoi <tudorilisoi@yahoo.com>
2018-07-10 11:22:02 +0300

    code comments

--- server/helpers.js
+++ server/helpers.js
@@ -1,4 +1,7 @@
 
+// this wraps an async express route handler function into a try-catch
+// it returns a new function which handles failures gracefully 
+// by sending a JSON encoded message and a HTTP 500 (internal server error) status code
 function expressTryCatchWrapper(fn) {
     return async function (req, resp) {
         try {
